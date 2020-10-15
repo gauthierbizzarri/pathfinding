ebf5781139acac8bf05e81eee5678d5706d19e0c
Douraïd BENHASSEN <douraid.ben-hassen@imerir.com>
2020-10-15 08:07:05 +0200

    Verification de la bonne trajectoire Romain Douraïd

--- laser_game.py
+++ laser_game.py
@@ -11,6 +11,8 @@ on veut ax
 """
 import matplotlib.pyplot as plt
 
+graph = plt #objet plt
+
 def calcule_A(pxc, pyc):
     a = 0.0
     a = float(pyc)/float(pxc)
@@ -18,14 +20,32 @@ def calcule_A(pxc, pyc):
     liste_de_point_ord = []
     liste_de_point_abs = []
     
+    ordplus = 0.0
+    absplus = pxc - 1 #valeur max des abs = pxc - 1
+    
     for i in range(1, pxc):
-        liste_de_point_abs.append(i)
+        liste_de_point_abs.append(i) #append sur les abs
 
     for i in range(1, pxc):
         a = float(i)/float(a)
-        liste_de_point_ord.append(a*i)
+        liste_de_point_ord.append(a*i) #append sur les ord
+        if ordplus < (a*i): #condition pour obtenir la valeur max des ord
+            ordplus = a*i
     
-#    return liste_de_point_ord,liste_de_point_abs
- 
-    plt.plot(liste_de_point_abs, liste_de_point_ord)
-    plt.show(())
+    graph.plot(liste_de_point_abs, liste_de_point_ord) #trace la courbe des points
+    return absplus, ordplus #retourne les coordonnées du dernier point de la courbe
+
+def check_A(pxc, pyc, px, py):
+    coord = calcule_A(pxc, pyc) #récupération des coordonnées de pxc & pyx
+    result_color = '' #couleur en fonction du resultat: gagnié = vert | raté = rouge
+    
+    if coord[0] == px and coord[1]== py: #comparaison avec les coordonnées utilisateur
+        print("gagné")
+        result_color = 'green'
+    else:
+        print("raté")
+        result_color = 'red'
+        
+    graph.scatter(px,py, s=30, c=result_color, marker='X') #affichage de la cible
+    graph.plot([1,coord[0]], [1,coord[1]], "-", c=result_color) #affichage trajectoire
+
